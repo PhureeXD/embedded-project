22bcdd331932df1b1634b808f3c99b0a2e2a6605
Phuree <phureeppp@gmail.com>
2024-12-01 13:49:33 +0700

    Add Skeleton component and implement Suspense for loading states in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import LedButtonMonitor from "@/components/LedButtonMonitor"
 import { DataTableDemo } from "@/components/DataTableDemo"
 import { AddDataForm } from "@/components/AddDataForm"
 import LineChart from "@/components/LineChart"
+import { Suspense } from "react"
+import { Skeleton } from "@/components/ui/skeleton"
+import Dashboard from "@/components/Dashboard"
 
 export default function Home() {
   return (
@@ -11,19 +14,33 @@ export default function Home() {
       </h1>
       <section>
         <article className="md:w-[40dvw] mx-auto mt-8">
-          <LedButtonMonitor />
+          <Suspense fallback={<Skeleton className="h-40" />}>
+            <LedButtonMonitor />
+          </Suspense>
         </article>
 
         <article className="md:w-[40dvw] mx-auto mt-8">
-          <AddDataForm />
+          <Suspense fallback={<Skeleton className="h-40" />}>
+            <AddDataForm />
+          </Suspense>
         </article>
 
         <article className="md:w-[60dvw] mx-auto mt-8">
-          <DataTableDemo />
+          <Suspense fallback={<Skeleton className="h-60" />}>
+            <DataTableDemo />
+          </Suspense>
+        </article>
+
+        <article className="max-w-[90dvw] mx-auto mt-8">
+          <Suspense fallback={<Skeleton className="h-80" />}>
+            <Dashboard />
+          </Suspense>
         </article>
 
         <article className="max-w-[70dvw] mx-auto mt-8">
-          <LineChart />
+          <Suspense fallback={<Skeleton className="h-80" />}>
+            <LineChart />
+          </Suspense>
         </article>
       </section>
     </main>
