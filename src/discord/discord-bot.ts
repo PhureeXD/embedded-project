2137c2ba6ef584d20554322d8aee86398da70347
Phuree <phureeppp@gmail.com>
2024-12-01 12:47:51 +0700

    Enhance startDiscordBot function to support dynamic field updates and improved timestamp handling

--- src/discord/discord-bot.ts
+++ src/discord/discord-bot.ts
@@ -9,7 +9,12 @@ import {
 
 let discordClient: Client | null = null
 
-export async function startDiscordBot(ledStatus: boolean) {
+export async function startDiscordBot(
+  field: string,
+  value: number | string,
+  date: Date,
+  ledStatus?: boolean,
+) {
   if (!discordClient) {
     discordClient = new Client({
       intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
@@ -18,13 +23,21 @@ export async function startDiscordBot(ledStatus: boolean) {
     await discordClient.login(process.env.DISCORD_BOT_TOKEN)
   }
 
-  discordClient.once(Events.ClientReady, async () => {
-    const channel = discordClient!.channels.cache.get(
-      process.env.NEXT_PUBLIC_DISCORD_CHANNEL_ID!,
-    )
+  if (!discordClient.isReady()) {
+    await new Promise<void>((resolve) => {
+      discordClient!.once(Events.ClientReady, () => resolve())
+    })
+  }
 
-    if (channel instanceof TextChannel) {
-      const embed = new EmbedBuilder()
+  const channel = discordClient!.channels.cache.get(
+    process.env.NEXT_PUBLIC_DISCORD_CHANNEL_ID!,
+  )
+
+  const embed = new EmbedBuilder()
+
+  switch (field) {
+    case "ledStatus":
+      embed
         .setColor(ledStatus ? 0x00ff00 : 0xff0000)
         .setTitle("LED Status Update")
         .setDescription(`The LED status has been updated.`)
@@ -32,9 +45,65 @@ export async function startDiscordBot(ledStatus: boolean) {
           name: "New Status",
           value: ledStatus ? "✅ On" : "❌ Off",
         })
-        .setTimestamp()
+        .setTimestamp(date)
+      break
 
-      await channel.send({ embeds: [embed] })
-    }
-  })
+    case "ldr":
+      if (+value <= 1500 && +value > 400) return
+
+      embed
+        .setColor(+value <= 400 ? 0xff0000 : 0x00ff00)
+        .setTitle("LDR Value Update")
+        .setDescription(`The LDR value has been updated.`)
+        .addFields({
+          name: "New Value",
+          value: value.toString(),
+        })
+        .setTimestamp(date)
+      break
+
+    case "dist":
+      if (+value > 100) return
+
+      embed
+        .setColor(0x00ff00)
+        .setTitle("Distance Value Update")
+        .setDescription(`The distance value has been updated.`)
+        .addFields({
+          name: "New Value",
+          value: value.toString(),
+        })
+        .setTimestamp(date)
+      break
+
+    case "smoke":
+      if (+value < 500) return
+
+      embed
+        .setColor(0xff0000)
+        .setTitle("Smoke Value Update")
+        .setDescription(`The smoke value has been updated.`)
+        .addFields({
+          name: "New Value",
+          value: value.toString(),
+        })
+        .setTimestamp(date)
+      break
+
+    case "payments":
+      embed
+        .setColor(0x00f000)
+        .setTitle("Task Update")
+        .setDescription(`The task value has been updated.`)
+        .addFields({
+          name: "New Value",
+          value: value.toString(),
+        })
+        .setTimestamp(date)
+
+    default:
+      break
+  }
+  console.log(embed)
+  await (channel as TextChannel).send({ embeds: [embed] })
 }
